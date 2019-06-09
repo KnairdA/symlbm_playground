71a678256d71d3942d040bbbe42d6a0270feb3cc
Adrian Kummerlaender <adrian@kummerlaender.eu>
2019-06-09 23:57:04 +0200

    First test of partially generated LBM kernel
    A kernel extracted from `lbn_codegen.ipynb` yields ~665 MLUPS compared
    to the ~600 MLUPS produced by a manually optimized kernel.
    
    Note that this new kernel currently doesn't handle boundary conditions (but
    dropping in a density condition doesn't impact performance).

--- codegen_lbm.py
+++ codegen_lbm.py
@@ -9,12 +9,6 @@ import matplotlib.pyplot as plt
 import time
 
 kernel = """
-float constant w[9] = {
-    1./36., 1./9., 1./36.,
-    1./9. , 4./9., 1./9. ,
-    1./36 , 1./9., 1./36.
-};
-
 unsigned int indexOfDirection(int i, int j) {
     return (i+1) + 3*(1-j);
 }
@@ -32,18 +26,6 @@ __global float f_i(__global __read_only float* f, int x, int y, int i, int j) {
     return f[idx(x,y,i,j)];
 }
 
-float comp(int i, int j, float2 v) {
-    return i*v.x + j*v.y;
-}
-
-float sq(float x) {
-    return x*x;
-}
-
-float f_eq(float w, float d, float2 v, int i, int j, float dotv) {
-    return w * d * (1.f + 3.f*comp(i,j,v) + 4.5f*sq(comp(i,j,v)) - 1.5f*dotv);
-}
-
 __kernel void collide_and_stream(__global __write_only float* f_a,
                                  __global __read_only  float* f_b,
                                  __global __write_only float* moments,
@@ -59,57 +41,64 @@ __kernel void collide_and_stream(__global __write_only float* f_a,
         return;
     }
 
-    float f0 = f_i(f_b, cell.x+1, cell.y-1, -1, 1);
-    float f1 = f_i(f_b, cell.x  , cell.y-1,  0, 1);
-    float f2 = f_i(f_b, cell.x-1, cell.y-1,  1, 1);
-    float f3 = f_i(f_b, cell.x+1, cell.y  , -1, 0);
-    float f4 = f_i(f_b, cell.x  , cell.y  ,  0, 0);
-    float f5 = f_i(f_b, cell.x-1, cell.y  ,  1, 0);
-    float f6 = f_i(f_b, cell.x+1, cell.y+1, -1,-1);
-    float f7 = f_i(f_b, cell.x  , cell.y+1,  0,-1);
-    float f8 = f_i(f_b, cell.x-1, cell.y+1,  1,-1);
+    const float f_curr_0 = f_i(f_b, cell.x+1, cell.y-1, -1, 1);
+    const float f_curr_1 = f_i(f_b, cell.x  , cell.y-1,  0, 1);
+    const float f_curr_2 = f_i(f_b, cell.x-1, cell.y-1,  1, 1);
+    const float f_curr_3 = f_i(f_b, cell.x+1, cell.y  , -1, 0);
+    const float f_curr_4 = f_i(f_b, cell.x  , cell.y  ,  0, 0);
+    const float f_curr_5 = f_i(f_b, cell.x-1, cell.y  ,  1, 0);
+    const float f_curr_6 = f_i(f_b, cell.x+1, cell.y+1, -1,-1);
+    const float f_curr_7 = f_i(f_b, cell.x  , cell.y+1,  0,-1);
+    const float f_curr_8 = f_i(f_b, cell.x-1, cell.y+1,  1,-1);
 
-    const float d = f0 + f1 + f2 + f3 + f4 + f5 + f6 + f7 + f8;
+    const float x0 = f_curr_0 + f_curr_1 + f_curr_2 + f_curr_3 + f_curr_4 + f_curr_5 + f_curr_6 + f_curr_7 + f_curr_8;
+    const float x1 = 2*f_curr_0;
+    const float x2 = 2*f_curr_8;
+    const float x3 = -f_curr_3 + f_curr_5;
+    const float x4 = pow(x0, -2);
+    const float x5 = 9*x4;
+    const float x6 = f_curr_0 - f_curr_8;
+    const float x7 = f_curr_1 - f_curr_7;
+    const float x8 = f_curr_2 - f_curr_6;
+    const float x9 = x6 + x7 + x8;
+    const float x10 = 6/x0;
+    const float x11 = x10*x9;
+    const float x12 = f_curr_3 - f_curr_5;
+    const float x13 = -f_curr_2 + f_curr_6 + x12 + x6;
+    const float x14 = pow(x13, 2);
+    const float x15 = 3*x4;
+    const float x16 = -x14*x15 + 2;
+    const float x17 = x11 + x16;
+    const float x18 = pow(x9, 2);
+    const float x19 = x15*x18;
+    const float x20 = -x19;
+    const float x21 = x10*x13;
+    const float x22 = x20 + x21;
+    const float x23 = 1.0/$tau;
+    const float x24 = (1.0/72.0)*x23;
+    const float x25 = 6*x4;
+    const float x26 = x18*x25;
+    const float x27 = (1.0/18.0)*x23;
+    const float x28 = x5*pow(2*f_curr_2 - 2*f_curr_6 + x3 + x7, 2);
+    const float x29 = x20 - x21;
+    const float x30 = x14*x25 + 2;
+    const float x31 = -f_curr_0 + f_curr_8 + x3 + x8;
+    const float x32 = x15*pow(x31, 2) - 2;
+    const float x33 = x19 + x32;
 
-    float2 v = (float2)(
-        (f5 - f3 + f2 - f6 + f8 - f0) / d,
-        (f1 - f7 + f2 - f6 - f8 + f0) / d
-    );
+    f_a[0*$nCells + gid] = f_curr_0 - x24*(72*f_curr_0 - x0*(x17 + x22 + x5*pow(-f_curr_1 + f_curr_7 - x1 + x2 + x3, 2)));
+    f_a[1*$nCells + gid] = f_curr_1 - x27*(18*f_curr_1 - x0*(x17 + x26));
+    f_a[2*$nCells + gid] = f_curr_2 - x24*(72*f_curr_2 - x0*(x17 + x28 + x29));
+    f_a[3*$nCells + gid] = f_curr_3 - x27*(18*f_curr_3 - x0*(x22 + x30));
+    f_a[4*$nCells + gid] = f_curr_4 - 1.0/9.0*x23*(9*f_curr_4 + 2*x0*x33);
+    f_a[5*$nCells + gid] = f_curr_5 - x27*(18*f_curr_5 - x0*(x29 + x30));
+    f_a[6*$nCells + gid] = f_curr_6 - x24*(72*f_curr_6 + x0*(x10*x31 + x11 - x28 + x33));
+    f_a[7*$nCells + gid] = f_curr_7 - x27*(18*f_curr_7 + x0*(x11 - x26 + x32));
+    f_a[8*$nCells + gid] = f_curr_8 - x24*(72*f_curr_8 - x0*(-x11 + x16 + x29 + x5*pow(x1 + x12 - x2 + x7, 2)));
 
-    if ( m == 2 ) {
-        v = (float2)(0.0f, 0.0f);
-    }
-
-    if ( m == 3 ) {
-        v = (float2)(0.1f, 0.0f);
-    }
-
-    const float dotv = dot(v,v);
-
-    f0 += $omega * (f_eq(w[0], d,v,-1, 1, dotv) - f0);
-    f1 += $omega * (f_eq(w[1], d,v, 0, 1, dotv) - f1);
-    f2 += $omega * (f_eq(w[2], d,v, 1, 1, dotv) - f2);
-    f3 += $omega * (f_eq(w[3], d,v,-1, 0, dotv) - f3);
-    f4 += $omega * (f_eq(w[4], d,v, 0, 0, dotv) - f4);
-    f5 += $omega * (f_eq(w[5], d,v, 1, 0, dotv) - f5);
-    f6 += $omega * (f_eq(w[6], d,v,-1,-1, dotv) - f6);
-    f7 += $omega * (f_eq(w[7], d,v, 0,-1, dotv) - f7);
-    f8 += $omega * (f_eq(w[8], d,v, 1,-1, dotv) - f8);
-
-    f_a[0*$nCells + gid] = f0;
-    f_a[1*$nCells + gid] = f1;
-    f_a[2*$nCells + gid] = f2;
-    f_a[3*$nCells + gid] = f3;
-    f_a[4*$nCells + gid] = f4;
-    f_a[5*$nCells + gid] = f5;
-    f_a[6*$nCells + gid] = f6;
-    f_a[7*$nCells + gid] = f7;
-    f_a[8*$nCells + gid] = f8;
-
-    moments[0*$nCells + gid] = d;
-    moments[1*$nCells + gid] = v.x;
-    moments[2*$nCells + gid] = v.y;
+    moments[gid] = x0;
 }"""
+
 
 class D2Q9_BGK_Lattice:
     def idx(self, x, y):
@@ -134,6 +123,7 @@ class D2Q9_BGK_Lattice:
         self.setup_geometry()
 
         self.equilibrilize()
+        self.setup_anomaly()
 
         self.cl_pop_a = cl.Buffer(self.context, mf.READ_WRITE | mf.USE_HOST_PTR, hostbuf=self.np_pop_a)
         self.cl_pop_b = cl.Buffer(self.context, mf.READ_WRITE | mf.USE_HOST_PTR, hostbuf=self.np_pop_b)
@@ -152,9 +142,6 @@ class D2Q9_BGK_Lattice:
                 else:
                     self.np_material[self.idx(x,y)] = 1
 
-        for x in range(1,self.nX-1):
-            self.np_material[self.idx(x,1)] = 3
-
     def equilibrilize(self):
         self.np_pop_a[(0,2,6,8),:] = 1./36.
         self.np_pop_a[(1,3,5,7),:] = 1./9.
@@ -164,12 +151,25 @@ class D2Q9_BGK_Lattice:
         self.np_pop_b[(1,3,5,7),:] = 1./9.
         self.np_pop_b[4,:] = 4./9.
 
+    def setup_anomaly(self):
+        bubbles = [ [        self.nX//4,        self.nY//4],
+                    [        self.nX//4,self.nY-self.nY//4],
+                    [self.nX-self.nX//4,        self.nY//4],
+                    [self.nX-self.nX//4,self.nY-self.nY//4] ]
+
+        for x in range(0,self.nX-1):
+            for y in range(0,self.nY-1):
+                for [a,b] in bubbles:
+                    if numpy.sqrt((x-a)*(x-a)+(y-b)*(y-b)) < self.nX//10:
+                        self.np_pop_a[:,self.idx(x,y)] = 1./24.
+                        self.np_pop_b[:,self.idx(x,y)] = 1./24.
+
     def build_kernel(self):
         self.program = cl.Program(self.context, Template(kernel).substitute({
             'nX' : self.nX,
             'nY' : self.nY,
             'nCells': self.nCells,
-            'omega': 1.0/0.56
+            'tau': '0.8f'
         })).build() #'-cl-single-precision-constant -cl-fast-relaxed-math')
 
     def evolve(self):
@@ -186,24 +186,24 @@ class D2Q9_BGK_Lattice:
     def show(self, i):
         cl.enqueue_copy(LBM.queue, LBM.np_moments, LBM.cl_moments).wait();
 
-        velocity = numpy.ndarray(shape=(self.nY-2, self.nX-2))
+        density = numpy.ndarray(shape=(self.nX-2, self.nY-2))
         for y in range(1,self.nY-1):
             for x in range(1,self.nX-1):
-                velocity[y-1,x-1] = numpy.sqrt(self.np_moments[1,self.idx(x,y)]**2 + self.np_moments[2,self.idx(x,y)]**2)
+                density[y-1,x-1] = self.np_moments[0,self.idx(x,y)]
 
-        plt.imshow(velocity, cmap=plt.get_cmap("seismic"))
-        plt.savefig("result/velocity_" + str(i) + ".png")
+        plt.imshow(density, vmin=0.2, vmax=2.0, cmap=plt.get_cmap("seismic"))
+        plt.savefig("result/density_" + str(i) + ".png")
 
 
 def MLUPS(cells, steps, time):
     return cells * steps / time * 1e-6
 
-nUpdates = 100000
-nStat = 1000
+nUpdates = 1000
+nStat = 100
 
 print("Initializing simulation...\n")
 
-LBM = D2Q9_BGK_Lattice(256, 256)
+LBM = D2Q9_BGK_Lattice(1024, 1024)
 
 print("Starting simulation using %d cells...\n" % LBM.nCells)
 
