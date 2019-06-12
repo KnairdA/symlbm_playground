9cf82f641b8982df526107b99d883545ee02fe20
Adrian Kummerlaender <adrian@kummerlaender.eu>
2019-06-12 21:01:42 +0200

    Restructuring

--- D2Q9.py
+++ D2Q9.py
@@ -2,18 +2,13 @@ import pyopencl as cl
 mf = cl.mem_flags
 
 import numpy
-import time
-
-import matplotlib
-import matplotlib.pyplot as plt
-matplotlib.use('AGG')
 
 import sympy
-import lbm_d2q9 as D2Q9
+import symbolic.D2Q9 as D2Q9
 
 from mako.template import Template
 
-class D2Q9_BGK_Lattice:
+class Lattice:
     def idx(self, x, y):
         return y * self.nX + x;
 
@@ -79,54 +74,5 @@ class D2Q9_BGK_Lattice:
             self.program.collect_moments(self.queue, (self.nX,self.nY), (32,1), self.cl_pop_b, self.cl_moments)
         else:
             self.program.collect_moments(self.queue, (self.nX,self.nY), (32,1), self.cl_pop_a, self.cl_moments)
-        cl.enqueue_copy(LBM.queue, moments, LBM.cl_moments).wait();
+        cl.enqueue_copy(self.queue, moments, self.cl_moments).wait();
         return moments
-
-
-def MLUPS(cells, steps, time):
-    return cells * steps / time * 1e-6
-
-def generate_moment_plots(lattice, moments):
-    for i, m in enumerate(moments):
-        print("Generating plot %d of %d." % (i+1, len(moments)))
-
-        density = numpy.ndarray(shape=(lattice.nY-2, lattice.nX-2))
-        for y in range(1,lattice.nY-1):
-            for x in range(1,lattice.nX-1):
-                density[y-1,x-1] = m[0,lattice.idx(x,y)]
-
-        plt.figure(figsize=(10, 10))
-        plt.imshow(density, origin='lower', vmin=0.2, vmax=2.0, cmap=plt.get_cmap('seismic'))
-        plt.savefig("result/density_" + str(i) + ".png", bbox_inches='tight', pad_inches=0)
-
-def box(nX, nY, x, y):
-    if x == 1 or y == 1 or x == nX-2 or y == nY-2:
-        return 2
-    else:
-        return 1
-
-nUpdates = 1000
-nStat    = 100
-
-moments = []
-
-print("Initializing simulation...\n")
-
-LBM = D2Q9_BGK_Lattice(nX = 1024, nY = 1024, tau = 0.8, geometry = box)
-
-print("Starting simulation using %d cells...\n" % LBM.nCells)
-
-lastStat = time.time()
-
-for i in range(1,nUpdates+1):
-    LBM.evolve()
-
-    if i % nStat == 0:
-        LBM.sync()
-        print("i = %4d; %3.0f MLUPS" % (i, MLUPS(LBM.nCells, nStat, time.time() - lastStat)))
-        moments.append(LBM.get_moments())
-        lastStat = time.time()
-
-print("\nConcluded simulation.\n")
-
-generate_moment_plots(LBM, moments)
